7da79fed9d88288dfdb36e11b58992a98c4086de
Konstantin Münster <konstantin.muenster@mail.com>
2019-12-21 19:07:15 +0100

    Add gatsby-plugin-netlify and _redirects file

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
