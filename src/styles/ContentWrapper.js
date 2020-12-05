b8cb7031af1d4471b9c17774b66a2652ff83b507
Konstantin Münster <konstantin.muenster@mail.com>
2020-12-05 20:45:08 +0100

    Delete duplicate files

--- src/styles/ContentWrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-const ContentWrapper = styled.div`
-  max-width: ${({ theme }) => theme.pageWidth};
-  margin: 0 auto;
-  padding: 0 2.5rem;
-`
-export default ContentWrapper
